18ed16dcef3f5615b1a13a654d89f13589572e62
Fl0Ch3v <65029165+Fl0Ch3v@users.noreply.github.com>
2020-11-27 09:15:08 +0100

    Update mesFonctions.js

--- mesFonctions.js
+++ mesFonctions.js
@@ -123,7 +123,7 @@ function initialize() {
 		// création d'un contrôle des couches pour modifier les couches de fond de plan	
 		var baseLayers = {
 			"OpenStreetMap": osmLayer,
-			"OSM_Géo2France" : osmG2FLayer
+			"OSM_Géo2France3" : osmG2FLayer
 		};
 
 		L.control.layers(baseLayers).addTo(map);
